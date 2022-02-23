548483114abd29d417ccae951951e57f77324fdf
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-02-23 14:48:24 +0100

    Add table with ui statuses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1>Hi!</h1> `,
-  styles: [],
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  form = new FormGroup({
+    firstName: new FormControl(null, [Validators.required]),
+    lastName: new FormControl(null, [Validators.required]),
+  });
+
+  get firstName(): FormControl {
+    return this.form.get('firstName') as FormControl;
+  }
+
+  get lastName(): FormControl {
+    return this.form.get('lastName') as FormControl;
+  }
+
+  resetAll() {
+    this.form.markAsUntouched();
+    this.form.markAsPristine();
+  }
+}
