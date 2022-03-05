cda1ef2f92c730fd2a56b5ba465b7fac7cdfb9d7
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-03-05 21:48:13 +0100

    Add age control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   form = new FormGroup({
     firstName: new FormControl(null, [Validators.required]),
     lastName: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.required]),
   });
 
   get firstName(): FormControl {
@@ -17,6 +18,10 @@ export class AppComponent {
 
   get lastName(): FormControl {
     return this.form.get('lastName') as FormControl;
+  }
+
+  get age(): FormControl {
+    return this.form.get('age') as FormControl;
   }
 
   resetAll() {
