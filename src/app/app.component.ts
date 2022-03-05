be8587124ea223aea974d96b34b6a03a93706c47
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-03-05 22:53:13 +0100

    Add checkbox for all fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,17 +25,23 @@ export class AppComponent implements OnDestroy {
     return this.form.get('email') as FormControl;
   }
 
+  toggleLoginControl = new FormControl(false);
+  togglePasswordControl = new FormControl(false);
   toggleEmailControl = new FormControl(false);
 
-  private toggleEmailSub = this.toggleEmailControl.valueChanges.subscribe(() => this.toggleEmail());
-
-  toggleEmail() {
-    const hours = this.form.get('email')!;
-    hours.enabled ? hours.disable() : hours.enable();
-  }
+  private toggleLoginSub = this.toggleLoginControl.valueChanges.subscribe(() => this.toggle('login'));
+  private togglePasswordSub = this.togglePasswordControl.valueChanges.subscribe(() => this.toggle('password'));
+  private toggleEmailSub = this.toggleEmailControl.valueChanges.subscribe(() => this.toggle('email'));
 
   ngOnDestroy(): void {
+    this.toggleLoginSub.unsubscribe();
+    this.togglePasswordSub.unsubscribe();
     this.toggleEmailSub.unsubscribe();
+  }
+
+  private toggle(control: string) {
+    const hours = this.form.get(control)!;
+    hours.enabled ? hours.disable() : hours.enable();
   }
 }
 
